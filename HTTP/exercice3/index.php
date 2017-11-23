1cc04f0540f3224b28cf1e156604c9d07c224a57
Unknown <camille.colvray@ynov.com>
2017-11-23 15:22:01 +0100

    Ex3

---
+++ HTTP/exercice3/index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Index</title>
+</head>
+<body>
+<?php echo '<p>Fichier index.php</p>'; ?>
+</body>
+</html>
---
+++ HTTP/exercice3/redirect.php
@@ -0,0 +1,11 @@
+html>
+<head>
+    <title>Redirect</title>
+</head>
+<body>
+<?php
+header('Location: http://127.0.0.1/frameworkWeb/Ynov-exercices/HTTP/exercice3/index.php');
+exit();
+?>
+</body>
+</html>
